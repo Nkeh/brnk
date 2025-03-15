5f9360aac47d2d8106e956ba5fab81d6435591db
Nkeh <bonyankeh@gmail.com>
2025-03-15 06:10:31 +0100

    Done with a minimal aboout section

--- frontend-angular/src/app/about/about.component.ts
+++ frontend-angular/src/app/about/about.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
+  
+  about_text: string = `Hey there! I’m BONYA Ransom Nkeh Kongnyuy, 
+  a Computer Engineering student passionate about Tech and Problem solving.
+  I work with Django, Angular, and AI models, but I’m also hands-on with electrical wiring, electrical appliance maintenance,
+  and troubleshooting—if it’s broken, I’ll figure out how to fix it!`
 
+  aboutText2: string = `Beyond coding, I enjoy designing with Canva and Adobe,
+  learning new things, and tackling real-world challenges. 
+  Whether it’s software, AI, or hardware, 
+  I love solving problems and building cool stuff. 
+  Let’s connect and create something amazing!`
 }
--- frontend-angular/src/app/app.component.ts
+++ frontend-angular/src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from './navbar/navbar.component';
 
+//my components
+import { NavbarComponent } from './navbar/navbar.component';
 import { Home1Component } from './home1/home1.component';
+import { AboutComponent } from './about/about.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavbarComponent, Home1Component],
+  imports: [CommonModule, RouterOutlet, NavbarComponent, Home1Component, AboutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
